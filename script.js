4dc99741f0b6b55d5f0cb448b988b591b980b964
FilipaMerino <filipaam@outlook.com>
2024-05-07 20:13:11 +0100

    Selecting and defining dice variables

--- script.js
+++ script.js
@@ -4,6 +4,13 @@
 const scorePlayer1 = document.getElementById('score--0');
 const scorePlayer2 = document.getElementById('score--1');
 let image = document.querySelector('.dice');
+
+const btnNew = document.querySelector('btn--new');
+
+const btnRoll = document.querySelector('btn--roll');
+
+const btnHold = document.querySelector('btn--hold');
+
 // ______________
 
 scorePlayer1.textContent = 0;
