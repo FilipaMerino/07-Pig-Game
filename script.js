5edd085242be7d48d0b04284d550180121b558e8
FilipaMerino <filipaam@outlook.com>
2024-05-07 19:31:10 +0100

    Change image according to dice

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+let diceNumber = Math.trunc(Math.random() * 6) + 1;
+console.log(diceNumber);
+
+let image = document.querySelector('.dice');
+console.log(image.src);
+
+switch (diceNumber) {
+  case 1:
+    image.src = 'dice-1.png';
+    break;
+  case 2:
+    image.src = 'dice-2.png';
+    break;
+  case 3:
+    image.src = 'dice-3.png';
+    break;
+  case 4:
+    image.src = 'dice-4.png';
+    break;
+  case 5:
+    image.src = 'dice-5.png';
+    break;
+  case 6:
+    image.src = 'dice-6.png';
+    break;
+  default:
+    image.src = 'dice-4.png';
+    break;
+}
