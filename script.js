004b54db8d20b8f310a2f7ff5afb9dc33461646b
FilipaMerino <filipaam@outlook.com>
2024-05-07 20:24:27 +0100

    show current player score

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // Selecting elements
 const scorePlayer1 = document.getElementById('score--0');
 const scorePlayer2 = document.getElementById('score--1');
+const currentPlayer1 = document.getElementById('current--0')
+const currentPlayer2 = document.getElementById('current--1')
+
 let image = document.querySelector('.dice');
 
 const btnNew = document.querySelector('btn--new');
@@ -13,18 +16,31 @@ const btnHold = document.querySelector('btn--hold');
 
 // ______________
 
+let currentScore = 0;
 scorePlayer1.textContent = 0;
 scorePlayer2.textContent = 0;
 image.classList.add('hidden');
 
+//Rolling dice Functionality
 const roleDice = function () {
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
   console.log(diceNumber);
   image.classList.remove('hidden');
   image.src = `dice-${diceNumber}.png`
+
+
+if (diceNumber !== 1){
+  currentScore += diceNumber;
+  currentPlayer1.textContent = currentScore
+} else {
+  // switch player
+}
+
+
 };
 
 
 
-// Rolling dice Functionality
+
+// Roll Dice when click button
 document.querySelector('.btn--roll').addEventListener('click', roleDice);
