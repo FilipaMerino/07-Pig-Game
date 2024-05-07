5fd54c9cf2e69b29dc50f21dbd0f189417be711f
FilipaMerino <filipaam@outlook.com>
2024-05-07 20:07:08 +0100

    Change dice according to image when roled the dice

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 'use strict';
 
-const scorePlayer1 = document.getElementById("score--0")
-
-const scorePlayer2 = document.getElementById("score--1")
-
-let diceNumber = Math.trunc(Math.random() * 6) + 1;
-console.log(diceNumber);
-
+// Selecting elements
+const scorePlayer1 = document.getElementById('score--0');
+const scorePlayer2 = document.getElementById('score--1');
 let image = document.querySelector('.dice');
-console.log(image.src);
+// ______________
 
-switch (diceNumber) {
-  case 1:
-    image.src = 'dice-1.png';
-    break;
-  case 2:
-    image.src = 'dice-2.png';
-    break;
-  case 3:
-    image.src = 'dice-3.png';
-    break;
-  case 4:
-    image.src = 'dice-4.png';
-    break;
-  case 5:
-    image.src = 'dice-5.png';
-    break;
-  case 6:
-    image.src = 'dice-6.png';
-    break;
-  default:
-    image.src = 'dice-4.png';
-    break;
-}
+scorePlayer1.textContent = 0;
+scorePlayer2.textContent = 0;
+image.classList.add('hidden');
+
+const roleDice = function () {
+  let diceNumber = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceNumber);
+  showDiceImage(diceNumber);
+};
+
+const showDiceImage = function (diceNumber) {
+  let image = document.querySelector('.dice');
+  switch (diceNumber) {
+    case 1:
+      image.classList.remove('hidden');
+      image.src = 'dice-1.png';
+      break;
+    case 2:
+      image.classList.remove('hidden');
+      image.src = 'dice-2.png';
+      break;
+    case 3:
+      image.classList.remove('hidden');
+      image.src = 'dice-3.png';
+      break;
+    case 4:
+      image.classList.remove('hidden');
+      image.src = 'dice-4.png';
+      break;
+    case 5:
+      image.classList.remove('hidden');
+      image.src = 'dice-5.png';
+      break;
+    case 6:
+      image.classList.remove('hidden');
+      image.src = 'dice-6.png';
+      break;
+    default:
+      image.src = '';
+      image.classList.add('hidden');
+      break;
+  }
+};
+
+document.querySelector('.btn--roll').addEventListener('click', roleDice);
