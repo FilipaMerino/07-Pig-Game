4b35b845da2c95369a56dd800d950c43d145b87f
FilipaMerino <filipaam@outlook.com>
2024-05-08 11:20:24 +0100

    Using toggle instead for background class

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ const scorePlayer1 = document.getElementById('score--0');
 const scorePlayer2 = document.getElementById('score--1');
 const currentPlayer1 = document.getElementById('current--0');
 const currentPlayer2 = document.getElementById('current--1');
+const player1 = document.querySelector(`.player--0`)
+const player2 = document.querySelector(`.player--1`)
+
 
 let image = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
 
-const btnNew = document.querySelector('btn--new');
-
-const btnRoll = document.querySelector('btn--roll');
-
-const btnHold = document.querySelector('btn--hold');
+const btnHold = document.querySelector('.btn--hold');
 
 const scores = [0, 0];
 let activePlayer = 0;
@@ -44,23 +45,22 @@ const roleDice = function () {
     // Reset score if rolls a 1
     document.getElementById(`current--${activePlayer}`).textContent = 0;
 
-    //Remove darker background color on Active Player
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player--active');
 
     // switch player
     document.getElementById(`current--${activePlayer}`).textContent = 0;
     document.getElementById(`score--${activePlayer}`).textContent = 0;
-    // currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0;
+    player1.classList.toggle("player--active")
+    player2.classList.toggle("player--active")
+
+    // currentScore = 0;
 
     //Add darker background color on Active Player
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player--active');
+
   }
 };
+
+
 
 // Roll Dice when click button
 document.querySelector('.btn--roll').addEventListener('click', roleDice);
