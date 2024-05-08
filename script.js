9b8f74f851f6a9c183b417052da29e366eb18e05
FilipaMerino <filipaam@outlook.com>
2024-05-08 12:07:23 +0100

    Show total score

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Selecting elements
+const player1 = document.querySelector(`.player--0`)
+const player2 = document.querySelector(`.player--1`)
 const scorePlayer1 = document.getElementById('score--0');
 const scorePlayer2 = document.getElementById('score--1');
 const currentPlayer1 = document.getElementById('current--0');
 const currentPlayer2 = document.getElementById('current--1');
-const player1 = document.querySelector(`.player--0`)
-const player2 = document.querySelector(`.player--1`)
 
 
 let image = document.querySelector('.dice');
@@ -14,18 +14,21 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+
+
+//STARTING CONDITIONS
 const scores = [0, 0];
 let activePlayer = 0;
 let currentScore = 0;
 scorePlayer1.textContent = 0;
 scorePlayer2.textContent = 0;
+
 image.classList.add('hidden');
 
 //SWITCH PLAYER FUNCTION
 const switchPlayer = function() {
   // switch player
   document.getElementById(`current--${activePlayer}`).textContent = 0;
-  document.getElementById(`score--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
   changeActivePlayerBackground()
   // currentScore = 0;
@@ -40,47 +43,40 @@ const changeActivePlayerBackground = function (){
 //Rolling dice Functionality
 const roleDice = function () {
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
-  console.log(diceNumber);
   image.classList.remove('hidden');
   image.src = `dice-${diceNumber}.png`;
 
-  activePlayer === 0
-    ? (scorePlayer1.textContent = diceNumber)
-    : (scorePlayer2.textContent = diceNumber);
+  // activePlayer === 0
+  //   ? (scorePlayer1.textContent = diceNumber)
+  //   : (scorePlayer2.textContent = diceNumber);
 
   if (diceNumber !== 1) {
+    //Add Dice to current score
     currentScore += diceNumber;
-
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
   } else {
-    // Reset score if rolls a 1
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
     // switch player
     switchPlayer()
   }
 };
 
-// Roll Dice when click button
-document.querySelector('.btn--roll').addEventListener('click', roleDice);
-
-
 
 const holdScore = function(){
-  //3. Switch to Next player
-    activePlayer === 0 ? 1 : 0;
-    changeActivePlayerBackground()
-
-// 1. Add current Score to active player's Score
-
+  // 1. Add current Score to active player's Score
   scores[activePlayer] += currentScore;
   // scores[1] = scores [1] + currentScore
-  document.getElementById(`current--${activePlayer}`).textContent = scores[activePlayer]
+  document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer]
 
-//2. Check if player's score is => 100;
-//Finish the game
 
+
+  //2. Check if player's score is => 100;
+  //Finish the game
+
+  //3. Switch to Next player
+  switchPlayer()
 
 }
 //Clicking on Hold Button
 btnHold.addEventListener("click", holdScore)
+// Roll Dice when click button
+document.querySelector('.btn--roll').addEventListener('click', roleDice);
